e7ebeaadc6d4a6ee5a0b48db22ca88364e95c5f4
Errol A. Zuñiga Jr <errol.zuniga@coredna.com>
2015-07-24 15:24:36 +0800

    Fix configs

--- config/autoload/canariumcore.global.php
+++ config/autoload/canariumcore.global.php
@@ -2,8 +2,10 @@
 
 return array(
     'canariumcore' => array(
-        'site_name'                  => 'Skeleton',
+        'site_name'                  => 'Canarium Skeleton App',
         'verbose_title'              => true,
         'is_authentication_required' => false,
-    )
+        'login_on_denied_access' => false,
+        'logout_third_party_login_too' => false
+    ),
 );
